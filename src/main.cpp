bf83a943d6ecc64c4627c6fba4131c647d28d5e5
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-04-07 15:14:49 -0400

    Experimental, random sampling makes things faster
    I implemented a very simple random samling to make things faster,
    but the program can give different outputs on an image, depending
    which pixels were taken into account, but generally it selects the
    "correct" color or a complementary color, more testing is required.

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,127 @@
 #include <Magick++.h>
 
+#include <bits/stdc++.h>
+#include <cmath>
 #include <iostream>
+#include <ostream>
 #include <random>
 #include <string>
 
-std::string getColor(double red, double green, double blue);
+enum Color
+{
+    BLACK,   // 0
+    BLUE,    // 1
+    GREEN,   // 2
+    CYAN,    // 3
+    RED,     // 4
+    MAGENTA, // 5
+    YELLOW,  // 6
+    WHITE,   // 7
+    ERROR    // 8
+};
+Color getColor(double hue);
+void count(double *color_counter, Color color, double saturation, double value);
+int getMaxColor(double *color_counter);
 
 int main(int argc, char **argv)
 {
     Magick::InitializeMagick("");
 
+    double color_counter[8];
+    memset(color_counter, 0, sizeof(color_counter));
+
     Magick::Image image;
-    double red = 0;
-    double green = 0;
-    double blue = 0;
     try
     {
-        image.read(argv[1]);
+        image.read("/home/ndfsa/Pictures/test/" + std::string(argv[1]));
         int imgWidth = image.columns();
         int imgHeight = image.rows();
         int channels = image.channels();
+        image.colorSpace(MagickCore::HSVColorspace);
+        // image.quantizeColors(4);
+        // image.quantize();
 
         std::random_device rd;
         std::mt19937 mt(rd());
         std::uniform_int_distribution<int> dist(1, 10);
 
-        const MagickCore::Quantum *pixel_cache = image.getConstPixels(0, 0, imgWidth, imgHeight);
+        MagickCore::Quantum *pixel_cache = image.getPixels(0, 0, imgWidth, imgHeight);
 
+        // for (int offset = 0; offset < 15; offset += channels)
         for (int offset = dist(mt); offset < imgHeight * imgWidth * channels; offset += channels * dist(mt))
         {
-            red += (double)pixel_cache[offset] / (imgHeight * imgWidth) * 5;
-            green += (double)pixel_cache[offset + 1] / (imgHeight * imgWidth) * 5;
-            blue += (double)pixel_cache[offset + 2] / (imgHeight * imgWidth) * 5;
+            // std::cout << pixel_cache[offset] * 255 / 65535 << " " << pixel_cache[offset + 1] * 255 / 65535 << " "
+            //          << pixel_cache[offset + 2] * 255 / 65535 << " " << std::endl;
+            // std::cout << pixel_cache[offset] * 360 / 65535 << " " << pixel_cache[offset + 1] * 100 / 65535 << " "
+            //          << pixel_cache[offset + 2] * 100 / 65535 << " " << std::endl;
+            Color aux = getColor(pixel_cache[offset]);
+            // std::cout << "color: " << aux << std::endl;
+            count(color_counter, aux, pixel_cache[offset + 1], pixel_cache[offset + 2]);
         }
-        std::cout << "R: " << red << " G: " << green << " B: " << blue << std::endl;
+        std::cout << getMaxColor(color_counter) << std::endl;
     }
     catch (Magick::Exception &err)
     {
         std::cout << "Caught exception: " << err.what() << std::endl;
         return 1;
     }
-    std::cout << getColor(red, green, blue) << std::endl;
     return 0;
 }
 
-std::string getColor(double red, double green, double blue)
+void count(double *color_counter, Color color, double saturation, double value)
 {
-    bool r = (red > 7000);
-    bool g = (green > 7000);
-    bool b = (blue > 7000);
-    if (r && g && b)
-        return "WHITE";
-    if (r && g && !b)
-        return "YELLOW";
-    if (r && !g && b)
-        return "MAGENTA";
-    if (r && !g && !b)
-        return "RED";
-    if (!r && g && b)
-        return "CYAN";
-    if (!r && g && !b)
-        return "GREEN";
-    if (!r && !g && b)
-        return "BLUE";
-    if (!r && !g && !b)
-        return "BLACK";
-    return "ERROR";
+    int s = (value > 1966 ? (saturation > 2621 ? color : 7) : 0);
+    color_counter[s] += saturation * value / (65535.0 * 65535.0);
+}
+
+int getMaxColor(double *color_counter)
+{
+    double max = -1;
+    double maxi = -1;
+    for (int i = 0; i < 8; i++)
+    {
+        if (color_counter[i] > max)
+        {
+            max = color_counter[i];
+            maxi = i;
+        }
+        std::cout << color_counter[i] << std::endl;
+    }
+    return maxi;
+}
+
+Color getColor(double hue)
+{
+    if (hue <= 7463)
+    {
+        return Color::RED;
+    }
+    else if (hue <= 13107)
+    {
+        return Color::YELLOW;
+    }
+    else if (hue <= 26031)
+    {
+        return Color::GREEN;
+    }
+    else if (hue <= 36954)
+    {
+        return Color::CYAN;
+    }
+    else if (hue <= 46784)
+    {
+        return Color::BLUE;
+    }
+    else if (hue <= 60801)
+    {
+        return Color::MAGENTA;
+    }
+    else if (hue <= 65535)
+    {
+        return Color::RED;
+    }
+    else
+    {
+        return Color::ERROR;
+    }
 }
